dec5a36491dcc391554295aca1041ff5ef642027
mrezavasebi <reza.vaseb@gmail.com>
2024-01-07 01:13:15 +0330

    new colors added

--- ui-config/colors.tsx
+++ ui-config/colors.tsx
@@ -1,8 +1,14 @@
 export const colors = {
+  red: "#D83F31",
   white: "white",
   black: "black",
+  green: "#35A29F",
   purple: "purple",
   yellow: "#ffc300",
   bgColor: "#F2F1EB",
   darkBlue: "#264653",
+
+  borderColor: {
+    white: "white",
+  },
 };
